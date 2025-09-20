10fae373636231efb4b46aa6104d2aae730e2537
nanaios <game.ryd.131@gmail.com>
2025-09-20 23:06:02 +0900

    モデル表示できた

--- src/main/java/com/CompactMekanismMachines/client/CompactClientRegistration.java
+++ src/main/java/com/CompactMekanismMachines/client/CompactClientRegistration.java
@@ -5,14 +5,18 @@ import com.CompactMekanismMachines.client.gui.GuiCompactIndustrialTurbine;
 import com.CompactMekanismMachines.client.gui.GuiCompactThermalEvaporation;
 import com.CompactMekanismMachines.client.render.CompressedWindGenerator.RenderCompressedWindGenerator_x2;
 import com.CompactMekanismMachines.common.CompactMekanismMachines;
+import com.CompactMekanismMachines.common.registries.CompactBlocks;
 import com.CompactMekanismMachines.common.registries.CompactContainerTypes;
 import com.CompactMekanismMachines.common.registries.CompactTileEntityTypes;
 import mekanism.client.ClientRegistrationUtil;
+import mekanism.client.render.RenderPropertiesProvider;
+import mekanism.generators.client.render.item.RenderWindGeneratorItem;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 
 @EventBusSubscriber(modid = CompactMekanismMachines.MOD_ID, value = Dist.CLIENT)
 public class CompactClientRegistration {
@@ -25,5 +29,10 @@ public class CompactClientRegistration {
         ClientRegistrationUtil.registerScreen(event, CompactContainerTypes.COMPACT_FISSION_REACTOR, GuiCompactFissionReactor::new);
         ClientRegistrationUtil.registerScreen(event, CompactContainerTypes.COMPACT_INDUSTRIAL_TURBINE, GuiCompactIndustrialTurbine::new);
         ClientRegistrationUtil.registerScreen(event, CompactContainerTypes.COMPACT_THERMAL_EVAPORATION, GuiCompactThermalEvaporation::new);
+    }
+
+    @SubscribeEvent
+    public static void registerClientExtensions(RegisterClientExtensionsEvent event) {
+        event.registerItem(new RenderPropertiesProvider.MekRenderProperties(RenderWindGeneratorItem.RENDERER), CompactBlocks.WIND_GENERATOR_X2.getItemHolder());
     }
 }
--- src/main/java/com/CompactMekanismMachines/common/registries/CompactBlocks.java
+++ src/main/java/com/CompactMekanismMachines/common/registries/CompactBlocks.java
@@ -5,14 +5,19 @@ import com.CompactMekanismMachines.common.tile.CompressedWindGenerator.TileEntit
 import com.CompactMekanismMachines.common.tile.TileEntityCompactFissionReactor;
 import com.CompactMekanismMachines.common.tile.TileEntityCompactIndustrialTurbine;
 import com.CompactMekanismMachines.common.tile.TileEntityCompactThermalEvaporation;
+import mekanism.common.attachments.containers.ContainerType;
+import mekanism.common.attachments.containers.item.ItemSlotsBuilder;
 import mekanism.common.block.interfaces.IHasDescription;
 import mekanism.common.block.prefab.BlockTile.BlockTileModel;
 import mekanism.common.content.blocktype.Machine;
+import mekanism.common.item.block.ItemBlockMekanism;
 import mekanism.common.item.block.ItemBlockTooltip;
 import mekanism.common.registration.impl.BlockDeferredRegister;
 import mekanism.common.registration.impl.BlockRegistryObject;
 import mekanism.common.resource.BlockResourceInfo;
 import mekanism.generators.common.content.blocktype.Generator;
+import net.minecraft.client.renderer.ItemModelShaper;
+import net.minecraft.data.models.ItemModelGenerators;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.MapColor;
 
@@ -43,27 +48,23 @@ public class CompactBlocks {
             ItemBlockTooltip<BlockTileModel<TileEntityCompressedWindGenerator_x2, Generator<TileEntityCompressedWindGenerator_x2>>>
             > WIND_GENERATOR_X2;
     static {
-        COMPACT_FISSION_REACTOR = registerTooltipBlock(
+        COMPACT_FISSION_REACTOR = BLOCKS.registerDetails(
                 "compact_fission_reactor",
                 () -> new BlockTileModel<>(CompactBlockTypes.COMPACT_FISSION_REACTOR, properties -> properties.mapColor(BlockResourceInfo.STEEL.getMapColor()).strength(0.2F))
         );
 
-        COMPACT_INDUSTRIAL_TURBINE = registerTooltipBlock(
+        COMPACT_INDUSTRIAL_TURBINE = BLOCKS.registerDetails(
                 "compact_industrial_turbine",
                 () -> new BlockTileModel<>(CompactBlockTypes.COMPACT_INDUSTRIAL_TURBINE, properties -> properties.mapColor(BlockResourceInfo.STEEL.getMapColor()))
         );
-        COMPACT_THERMAL_EVAPORATION = registerTooltipBlock(
+        COMPACT_THERMAL_EVAPORATION = BLOCKS.registerDetails(
                 "compact_thermal_evaporation",
                 () -> new BlockTileModel<>(CompactBlockTypes.COMPACT_THERMAL_EVAPORATION, properties -> properties.mapColor(BlockResourceInfo.STEEL.getMapColor()).strength(0.2F))
         );
         WIND_GENERATOR_X2 = BLOCKS.registerDetails(
                 "compressed_wind_generator_x2",
                 () -> new BlockTileModel<>(CompactBlockTypes.WIND_GENERATOR_X2, properties -> properties.mapColor(MapColor.METAL))
-        );
+        ).forItemHolder(holder -> holder.addAttachmentOnlyContainers(ContainerType.ITEM, () -> ItemSlotsBuilder.builder().addEnergy().build()));
 
-    }
-
-    private static <BLOCK extends Block & IHasDescription> BlockRegistryObject<BLOCK, ItemBlockTooltip<BLOCK>> registerTooltipBlock(String name, Supplier<BLOCK> blockCreator) {
-        return BLOCKS.register(name, blockCreator, ItemBlockTooltip::new);
     }
 }
